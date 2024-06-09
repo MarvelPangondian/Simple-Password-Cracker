cd8b67370d7d95c89c2a7031acf942d3e20d8fdd
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-06-09 21:19:43 +0700

    feat: simple brute force algorithm, update estimate_crack_time function

--- src/brute_force_password__cracker.py
+++ src/brute_force_password__cracker.py
@@ -1,13 +1,36 @@
-from utils import hash_password,estimate_crack_time
+from utils import hash_password,estimate_crack_time,possible_combinations
 import string
+import time
+from typing import Generator, Iterable, List, Optional, Tuple, Union
 
-def possible_combinations(characters, repeat):
-    pools = [list(characters)] * repeat
-    result = [[]]
-    for pool in pools:
-        result = [x + [y] for x in result for y in pool]
-    for prod in result:
-        yield tuple(prod)
+def brute_force_password_cracker(hashed_target_password : string) -> Tuple[str, float]:
+    
+    # All possible characters
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    start_time = time.time()
+    
+    # set maximum length of password to 5, for 6 > passwords it will take days to compute
+    for length in range(1, 6):
+        for password_tuple in possible_combinations(characters, length):
+            password = ''.join(password_tuple)
+            hashed_password = hash_password(password)
+            if hashed_password == hashed_target_password:
+                print(f"Password cracked: {password}")
+                end_time = time.time()
+                elapsed_time_seconds = end_time - start_time
+                return password,elapsed_time_seconds
+                
+
+    end_time = time.time()
+    elapsed_time_seconds = end_time - start_time
+
+    
+    return None, elapsed_time_seconds
 
 
-estimate_crack_time(string.ascii_letters + string.digits + string.punctuation, 4, 1_000_000) 
+# brute_force_password_cracker("asdasdgfsdfasdasd")
+password, time_taken = brute_force_password_cracker(hash_password("abc"))
+print("Password : " + str(password))
+print("Time taken : " + str(time_taken ) + " seconds")
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,13 @@
 import hashlib
-import string
-from typing import Tuple
+import time
+
+def possible_combinations(characters, repeat):
+    pools = [list(characters)] * repeat
+    result = [[]]
+    for pool in pools:
+        result = [x + [y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
@@ -9,11 +16,25 @@ def load_passwords(file_path):
     with open(file_path, 'r') as file:
         return [line.strip() for line in file]
     
-def estimate_crack_time(character_set: str, max_length: int, attempts_per_second: int) -> None:
+def estimate_crack_time(character_set: str, max_length: int) -> None:
     total_attempts = 0
     character_set_size = len(character_set)
     
-    # Calculate total number of attempt
+    # Calculate possible attempts per second
+    attempts = 0
+    start_time = time.time()
+    for length in range(1, 3 + 1):
+        for password_tuple in possible_combinations(character_set, length):
+            attempts += 1
+            password = ''.join(password_tuple)
+            hashed_password = hash_password(password)
+            if hashed_password == "ABCDEFGHIJ":
+                # simulate trying to break password
+                continue
+    end_time = time.time()
+    attempts_per_second = round(attempts / (end_time - start_time))
+    
+    # Calculate total number of attempts
     for length in range(1, max_length + 1):
         total_attempts += character_set_size ** length
     
@@ -21,21 +42,21 @@ def estimate_crack_time(character_set: str, max_length: int, attempts_per_second
     total_time_seconds = total_attempts / attempts_per_second
     
     # Display time according total_time_seconds
-    time = 0
+    time_end = 0
     unit = ""
     if total_time_seconds >= 24 * 3600:
-        time = total_time_seconds / (24 * 3600)
+        time_end = total_time_seconds / (24 * 3600)
         unit = "days"
     elif total_time_seconds >= 3600:
-        time = total_time_seconds / 3600
+        time_end = total_time_seconds / 3600
         unit = "hours"
     elif total_time_seconds >= 60:
-        time = total_time_seconds / 60
+        time_end = total_time_seconds / 60
         unit = "minutes"
     else:
-        time = total_time_seconds
+        time_end = total_time_seconds
         unit = "seconds"
-
     
-    print(f"Estimated time to crack (worst case): {time:.2f} {unit}")
+
+    print(f"Estimated time to crack (worst case): {time_end:.2f} {unit}")
     
